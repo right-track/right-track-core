0ba43fbad9f7a7fdb752f3c81befee19606b67be
David Waring <dev@davidwaring.net>
2018-01-14 15:58:09 -0500

    Search: Function reorganization

--- modules/search/search.js
+++ modules/search/search.js
@@ -15,6 +15,11 @@ const DateTime = require('../utils/DateTime.js');
 const CalendarTable = require('../query/CalendarTable.js');
 const LineGraphTable = require('../query/LineGraphTable.js');
 
+
+const LOG = true;
+
+
+
 /**
  * Perform a Trip Search between the origin and destination Stops
  * with the provided parameters and options
@@ -28,11 +33,11 @@ const LineGraphTable = require('../query/LineGraphTable.js');
  */
 function search(db, origin, destination, datetime, options, callback) {
 
-  console.log("====== STARTING SEARCH ======");
-  console.log("ORIGIN: " + origin.name);
-  console.log("DESTINATION: " + destination.name);
-  console.log("DATE/TIME: " + datetime.toString());
-  console.log("OPTIONS: " + JSON.stringify(options));
+  _log("====== STARTING SEARCH ======");
+  _log("ORIGIN: " + origin.name);
+  _log("DESTINATION: " + destination.name);
+  _log("DATE/TIME: " + datetime.toString());
+  _log("OPTIONS: " + JSON.stringify(options, null, 2));
 
 
   // List of Results
@@ -41,6 +46,10 @@ function search(db, origin, destination, datetime, options, callback) {
 
   // Get the initial Trip Search Dates
   _getTripSearchDates(db, datetime, options.preDateHours*60, options.postDateHours*60, function(err, tripSearchDates) {
+    _log("===== SEARCH TIME RANGE =====");
+    for ( let i = 0; i < tripSearchDates.length; i++ ) {
+      _log(JSON.stringify(tripSearchDates[i], null, 2));
+    }
 
     // Database Query Error
     if ( err ) {
@@ -49,12 +58,10 @@ function search(db, origin, destination, datetime, options, callback) {
 
 
     // Get the Initial Trips
-    _getTripsFromStop(db, origin, destination, origin, tripSearchDates, function(err, direct, indirect) {
-
-      console.log("======= INITIAL SEARCH ======");
-      console.log("DIRECT TRIPS: " + direct.length);
-      console.log("INDIRECT TRIPS: " + indirect.length);
-
+    _getTripsFromStop(db, origin, destination, origin, tripSearchDates, [], function(err, direct, indirect) {
+      _log("======= INITIAL TRIPS =======");
+      _log("DIRECT TRIPS: " + direct.length);
+      _log("INDIRECT TRIPS: " + indirect.length);
 
       // Database Query Error
       if ( err ) {
@@ -74,16 +81,16 @@ function search(db, origin, destination, datetime, options, callback) {
 
         // Add Result to List
         RESULTS.push(result);
-
       }
 
 
       // Process Indirect Trips, if transfers are allowed
       if ( indirect.length > 0 && options.allowTransfers ) {
-        console.log("====== TRANSFER SEARCH ======");
+        _log("====== TRANSFER SEARCH ======");
 
         // Process the Indirect Trips
         _processTrips(db, options, origin, destination, origin, indirect, [], function(err, results) {
+          _log("=== TRANSFER SEARCH RETURN ==");
 
           // Add results to final list
           RESULTS = RESULTS.concat(results);
@@ -113,20 +120,10 @@ function search(db, origin, destination, datetime, options, callback) {
    * @private
    */
   function _finish() {
-    console.log("=============================");
+    _log("=============================");
 
-    // TODO: clean results
-
-    // Print Results
-    for ( let i = 0; i < RESULTS.length; i++ ) {
-      let result = RESULTS[i];
-
-      console.log(result.origin.stop.name + ": " + result.origin.departure.getTimeReadable() + " --> " + result.destination.stop.name + ": " + result.destination.arrival.getTimeReadable());
-      if ( result.length > 1 ) {
-        _printSegments(result.segments);
-      }
-
-    }
+    // Clean the Results
+    RESULTS = _cleanResults(RESULTS);
 
     // Return the Results
     return callback(null, RESULTS);
@@ -137,61 +134,140 @@ function search(db, origin, destination, datetime, options, callback) {
 }
 
 
-function _processTrips(db, options, origin, destination, stop, trips, segments, callback) {
-  console.log("Processing " + trips.length + " Trips...");
+/**
+ * Clean the list of Trip Search Results
+ * - sort by departure date/time
+ * - pick best trip that leaves/arrives at same time
+ * - remove excessively long trips
+ * @param {TripSearchResult[]} results
+ * @returns {TripSearchResult[]} clean results
+ * @private
+ */
+function _cleanResults(results) {
+
+  // Sort by Departure Times
+  results.sort(TripSearchResult.sortByDeparture);
+
+  return results;
+
+}
+
+
+
+
+/**
+ * Process a group of Trips from the reference Stop
+ * @param {RightTrackDB} db The Right Track DB to query
+ * @param {Object} options Trip Search Options
+ * @param {Stop} origin Trip Search Origin Stop
+ * @param {Stop} destination Trip Search Destination Stop
+ * @param {Stop} enter Reference/Entry Stop
+ * @param {Trip[]} trips List of Trips to process
+ * @param {TripSearchResultSegment[]} segments Previously added Trip Search Result Segments
+ * @param {function} callback Callback function(err, results)
+ * @private
+ */
+function _processTrips(db, options, origin, destination, enter, trips, segments, callback) {
+
+  // Display function logs
+  _info();
 
   // Results to Return
   let RESULTS = [];
-
-  // Set Counters
-  let done = 0;
-  let count = trips.length;
 
   // Finish when there are no trips to process
   if ( trips.length === 0 ) {
     _finish();
   }
 
-  // Process each Trip individually
-  for ( let i = 0; i < trips.length; i++ ) {
-    _processTrip(db, options, origin, destination, origin, trips[i], [], function(err, results) {
-
-      // Database Query Error
-      if ( err ) {
-        return callback(err);
-      }
-
-      _finish(results);
-
-    });
+  // Start Processing the Trips
+  else {
+    _start();
   }
 
 
   /**
-   * Finished processing a Trip
+   * Start Processing a Trip
+   * @param index=0 Trip Index to Process
    * @private
    */
-  function _finish(results=[]) {
-    done ++;
+  function _start(index=0) {
+    if ( index < trips.length ) {
+      //console.warn("PROCESSING TRIP " + parseInt(index+1) + " / " + trips.length);
 
-    // Add results to final list
-    RESULTS = RESULTS.concat(results);
+      _processTrip(db, options, origin, destination, enter, trips[index], segments, function(err, results) {
 
-    if ( done === count ) {
-      return callback(null, RESULTS);
+        // Database Query Error
+        if ( err ) {
+          return callback(err);
+        }
+
+        // Add results to final list
+        RESULTS = RESULTS.concat(results);
+
+        // Next step
+        index = index+1;
+        _start(index);
+
+      });
+    }
+    else {
+      _finish();
+    }
+  }
+
+  /**
+   * Finished Processing the Trips
+   * @private
+   */
+  function _finish() {
+    return callback(null, RESULTS);
+  }
+
+  /**
+   * Display function logging info
+   * @private
+   */
+  function _info() {
+    if ( LOG ) {
+      let p = "";
+      for ( let i = 0; i < segments.length * 2; i++ ) {
+        p = p + " ";
+      }
+
+      let tripIds = [];
+      for ( let i = 0; i < trips.length; i++ ) {
+        tripIds.push(trips[i].id);
+      }
+      _log(p + "Trips From " + enter.name + ": " + trips.length + " ('" + tripIds.join("', '") + "')");
+
+      _log(p + "   Segments: " + segments.length);
+      _printSegments(segments, p + "   ");
     }
   }
 
 }
 
 
+/**
+ * Process a Trip from the reference/entry Stop
+ * @param {RightTrackDB} db The Right Track DB to query
+ * @param {Object} options Trip Search Options
+ * @param {Stop} origin Trip Search Origin Stop
+ * @param {Stop} destination Trip Search Destination Stop
+ * @param {Stop} enter Trip Entry Stop
+ * @param {Trip} trip Trip to process
+ * @param {TripSearchResultSegment[]} segments List of previously added Trip Search Result Segments
+ * @param {function} callback Callback function(err, results)
+ * @private
+ */
 function _processTrip(db, options, origin, destination, enter, trip, segments, callback) {
-  console.log("...Trip " + trip.id);
-  console.log("   Stop: " + enter.name);
-  console.log("   Segments: " + segments.length);
 
   // Get Transfer Stops for this Trip
   _getTransferStops(db, origin, destination, enter, trip, function(err, transfers) {
+
+    // Print Log Info
+    _info(transfers);
 
     // Database Query Error
     if ( err ) {
@@ -199,7 +275,7 @@ function _processTrip(db, options, origin, destination, enter, trip, segments, c
     }
 
     // Process the Transfers
-    _processTransfers(db, options, origin, destination, enter, transfers, trip, segments, function(err, results) {
+    _processStops(db, options, origin, destination, enter, transfers, trip, segments, function(err, results) {
 
       // Database Query Error
       if ( err ) {
@@ -213,65 +289,130 @@ function _processTrip(db, options, origin, destination, enter, trip, segments, c
 
   });
 
-}
 
-
-function _processTransfers(db, options, origin, destination, enter, transfers, trip, segments, callback) {
-  console.log("   Transfer Stops: " + transfers.length);
-
-  // Results to Return
-  let RESULTS = [];
-
-  // Set counters
-  let done = 0;
-  let count = transfers.length;
-
-  // Parse Each of the Transfer Stops
-  for ( let i = 0; i < transfers.length; i++ ) {
-    _processTransfer(db, options, origin, destination, enter, transfers[i], trip, segments, function(err, results) {
-
-      // Database Query Error
-      if ( err ) {
-        return callback(err);
+  /**
+   * Display logging info
+   * @private
+   */
+  function _info(stops) {
+    if ( LOG ) {
+      let p = "";
+      for ( let i = 0; i < segments.length * 2; i++ ) {
+        p = p + " ";
       }
 
-      // Process Results
-      _finish(results);
+      _log(p + "...Trip " + trip.id);
+      _log(p + "   Stop: " + enter.name + " @ " + trip.getStopTime(enter).departure.getTimeReadable());
+      _log(p + "   Segments: " + segments.length);
+      _printSegments(segments, p + "   ");
 
-    });
-  }
-
-
-  function _finish(results) {
-    done++;
-
-    // Add results to final list
-    RESULTS = RESULTS.concat(results);
-
-    if ( done === count ) {
-      return callback(null, RESULTS);
+      let stopNames = [];
+      for ( let i = 0; i < stops.length; i++ ) {
+        stopNames.push(stops[i].name);
+      }
+      _log(p + "   Transfer Stops: " + stops.length + " (" + stopNames.join(', ') + ")");
     }
   }
 
 }
 
 
-function _processTransfer(db, options, origin, destination, enter, transfer, trip, segments, callback) {
-  console.log("   ...Transfer @ " + transfer.name + " @ " + trip.getStopTime(transfer).arrival.getTimeReadable());
-
-  // Clone Segments
-  segments = segments.slice();
-
-  // Create the First Segment
-  let firstSegment = new TripSearchResultSegment(trip, enter, transfer);
-  segments.push(firstSegment);
+/**
+ * Process a group of Stops for possible transfers along a Trip from
+ * the reference/entry Stop
+ * @param {RightTrackDB} db The Right Track DB to query
+ * @param {Object} options Trip Search Options
+ * @param {Stop} origin Trip Search Origin Stop
+ * @param {Stop} destination Trip Search Destination Stop
+ * @param {Stop} enter Trip Reference/Entry Stop
+ * @param {Stop[]} transfers List of possible Transfer/Exit Stops
+ * @param {Trip} trip Trip being processed
+ * @param {TripSearchResultSegment[]} segments List of previously added Trip Search Result Segments
+ * @param {function} callback Callback function(err, results)
+ * @private
+ */
+function _processStops(db, options, origin, destination, enter, transfers, trip, segments, callback) {
 
   // Results to Return
   let RESULTS = [];
 
-  // Set Counters
-  let done = 0;
-  let count = 0;
+  // No transfers, finish
+  if ( transfers.length === 0 ) {
+    _finish();
+  }
+
+  // Start processing the Stops
+  else {
+    _start();
+  }
+
+
+  /**
+   * Start processing the Stop referenced by the index
+   * @param index=0 Stop Index
+   * @private
+   */
+  function _start(index=0) {
+    if ( index < transfers.length ) {
+      //console.warn("PROCESSING STOP " + parseInt(index+1) + " / " + transfers.length);
+
+      _processStop(db, options, origin, destination, enter, transfers[index], trip, segments, function(err, results) {
+
+        // Database Query Error
+        if ( err ) {
+          return callback(err);
+        }
+
+        // Add results to final list
+        RESULTS = RESULTS.concat(results);
+
+        // Start next Stop
+        index = index + 1;
+        _start(index);
+
+      })
+    }
+    else {
+      _finish();
+    }
+  }
+
+
+  /**
+   * Finished processing the Stops
+   * @private
+   */
+  function _finish() {
+    return callback(null, RESULTS);
+  }
+
+
+}
+
+
+/**
+ * Process a Stop as a possible Transfer Stop from the specified Trip
+ * @param {RightTrackDB} db The Right Track DB to query
+ * @param {Object} options Trip Search Options
+ * @param {Stop} origin Trip Search Origin Stop
+ * @param {Stop} destination Trip Search Destination Stop
+ * @param {Stop} enter Trip Entry Stop
+ * @param {Stop} transfer Trip Transfer/Exit Stop
+ * @param {Trip} trip Trip being processed
+ * @param {TripSearchResultSegment[]} segments List of previously added Trip Search Result Segments
+ * @param {function} callback Callback function(err, results)
+ * @private
+ */
+function _processStop(db, options, origin, destination, enter, transfer, trip, segments, callback) {
+
+  // Add first segment to list
+  let firstSegment = new TripSearchResultSegment(trip, enter, transfer);
+  segments = segments.slice();
+  segments.push(firstSegment);
+
+
+  // Results to Return
+  let RESULTS = [];
 
 
   // Set TripSearchDates
@@ -283,12 +424,17 @@ function _processTransfer(db, options, origin, destination, enter, transfer, tri
       return callback(err);
     }
 
+    // Exclude Trips already in the segments
+    let excludeTrips = [];
+    for ( let i = 0; i < segments.length; i++ ) {
+      excludeTrips.push(segments[i].trip.id);
+    }
+
     // Get Trips From Transfer
-    _getTripsFromStop(db, origin, destination, transfer, tripSearchDates, function(err, direct, indirect) {
-      console.log("      ...Trips From " + transfer.name + " From Trip " + trip.id + ":");
-      console.log("         Direct: " + direct.length);
-      console.log("         Indirect: " + indirect.length);
-      console.log("         Segments: " + segments.length);
+    _getTripsFromStop(db, origin, destination, transfer, tripSearchDates, excludeTrips, function(err, direct, indirect) {
+
+      // Print Stop Info
+      _info(direct, indirect);
 
       // Database Query Error
       if ( err ) {
@@ -315,16 +461,14 @@ function _processTransfer(db, options, origin, destination, enter, transfer, tri
 
       }
 
-
+      // TODO: Only process indirect trips if there are no direct trips?
       // Process Indirect Trips
       if ( indirect.length > 0 && options.allowTransfers && segments.length < options.maxTransfers ) {
-        console.log("====== CONTINUE TRANSFER SEARCH ======");
-
-        // Set counter
-        count = count + indirect.length;
 
         // Process the Indirect Trips
+        console.log("STARTING PROCESS TRIPS FROM TRANSFER LOOP");
         _processTrips(db, options, origin, destination, transfer, indirect, segments, function(err, results) {
+          console.log("RETURNING PROCESS TRIPS FROM TRANSFER LOOP");
 
           // Add results to final list
           RESULTS = RESULTS.concat(results);
@@ -339,7 +483,7 @@ function _processTransfer(db, options, origin, destination, enter, transfer, tri
 
       // No transfers required or disabled, finish
       else {
-        return callback(null, RESULTS);
+        _finish();
       }
 
     });
@@ -347,13 +491,27 @@ function _processTransfer(db, options, origin, destination, enter, transfer, tri
   });
 
 
-
   function _finish() {
-    done++;
-    if ( done === count ) {
-      return callback(null, RESULTS);
+    return callback(null, RESULTS);
+  }
+
+
+  function _info(direct, indirect) {
+    if ( LOG ) {
+      let p = "";
+      for ( let i = 0; i < segments.length * 3; i++ ) {
+        p = p + " ";
+      }
+
+      _log(p + "...Transfer @ " + transfer.name + " @ " + trip.getStopTime(transfer).arrival.getTimeReadable());
+      _log(p + "   ...Trips From " + transfer.name + " After Trip " + trip.id + ":");
+      _log(p + "      Direct: " + direct.length);
+      _log(p + "      Indirect: " + indirect.length);
+      _log(p + "   ...Segments: " + segments.length);
+      _printSegments(segments, p + "      ");
     }
   }
+
 
 }
 
@@ -422,10 +580,11 @@ function _getTransferStops(db, origin, destination, stop, trip, callback) {
  * @param {Stop} destination Trip Destination Stop
  * @param {Stop} stop Reference Stop
  * @param {TripSearchDate[]} tripSearchDates Trip Search Dates
+ * @param {String[]} excludeTrips List of Trip IDs to exclude
  * @param {function} callback Callback function(err, direct, indirect)
  * @private
  */
-function _getTripsFromStop(db, origin, destination, stop, tripSearchDates, callback) {
+function _getTripsFromStop(db, origin, destination, stop, tripSearchDates, excludeTrips, callback) {
 
   // Get all possible following stops
   LineGraphTable.getNextStops(db, origin.id, destination.id, stop.id, function(err, nextStops) {
@@ -449,11 +608,13 @@ function _getTripsFromStop(db, origin, destination, stop, tripSearchDates, callb
 
       // Sort Trips
       for ( let i = 0; i < trips.length; i++ ) {
-        if ( _tripGoesTo(trips[i], stop, destination) ) {
-          direct.push(trips[i]);
-        }
-        else {
-          indirect.push(trips[i]);
+        if ( excludeTrips.indexOf(trips[i].id) === -1 ) {
+          if ( _tripGoesTo(trips[i], stop, destination) ) {
+            direct.push(trips[i]);
+          }
+          else {
+            indirect.push(trips[i]);
+          }
         }
       }
 
@@ -589,6 +750,10 @@ function _getTripSearchDates(db, datetime, preMins, postMins, callback) {
 }
 
 
+
+
+
+
 /**
  * Check if the Trip goes from Origin --> Destination
  * @param {Trip} trip The Trip to check
@@ -613,10 +778,23 @@ function _tripGoesTo(trip, origin, destination) {
 }
 
 
-function _printSegments(segments) {
-  for ( let i = 0; i < segments.length; i++ ) {
-    let segment = segments[i];
-    console.log("  " + segment.enter.stop.name + " @ " + segment.enter.departure.getTimeReadable() + " --> " + segment.exit.stop.name + " @ " + segment.exit.arrival.getTimeReadable());
+
+
+
+
+function _printSegments(segments, padding="  ") {
+  if ( LOG ) {
+    for ( let i = 0; i < segments.length; i++ ) {
+      let segment = segments[i];
+      _log(padding + segment.enter.stop.name + " @ " + segment.enter.departure.getTimeReadable() + " --> " + segment.exit.stop.name + " @ " + segment.exit.arrival.getTimeReadable() + " (" + segment.trip.id + ")");
+    }
+  }
+}
+
+
+function _log(text) {
+  if ( LOG ) {
+    console.log(text);
   }
 }
 
